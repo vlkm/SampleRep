6d38be8a047819476dff50a671177ce05c5bd340
vlkm <46622384+vlkm@users.noreply.github.com>
2019-03-30 21:12:13 +0200

    Started Hello World

---
+++ src/main/java/SDA-tre/App.java
@@ -0,0 +1,13 @@
+package SDA-tre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
